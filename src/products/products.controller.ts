fc9e6ceef1358f2366f79c9f76cd173ee169c08e
Laishuxin <15622971239@163.com>
2022-07-02 21:46:21 +0800

    ci: add lint tools

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,7 +6,6 @@ import {
   Patch,
   Param,
   Delete,
-  NotFoundException,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
